635bb0c8756d8de7b3ae10b473cd687c361bb95d
Wojtek Nowak <billnewmann@gmail.com>
2020-05-09 20:43:21 +0100

    Finish migration of old files; modified files

--- public/front/js/settings.js
+++ public/front/js/settings.js
@@ -114,7 +114,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
+    url: '//' + window.location.hostname + (window.location.hostname==='localhost' ? ':3131/api' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
