d0c4deedb3400de1830dfb551db3bfc21511b959
Wojtek Nowak <billnewmann@gmail.com>
2020-05-10 16:37:20 +0100

    Add navigation and routing; modified files accordingly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import MainLayout from './components/layout/MainLayout/MainLayout';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Login from './components/views/Login/Login';
+import Dashboard from './components/views/Dashboard/Dashboard';
+import Tables from './components/views/Tables/Tables';
+import TablesBookingId from './components/views/Tables/TablesBookingId';
+import TablesBookingNew from './components/views/Tables/TablesBookingNew';
+import TablesEventsId from './components/views/Tables/TablesEventsId';
+import TablesEventsNew from './components/views/Tables/TablesEventsNew';
+import Waiter from './components/views/Waiter/Waiter';
+import WaiterOrderId from './components/views/Waiter/WaiterOrderId';
+import WaiterOrderNew from './components/views/Waiter/WaiterOrderNew';
+import Kitchen from './components/views/Kitchen/Kitchen';
 
 function App() {
   return (
-    <MainLayout>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React NOW
-          </a>
-        </header>
-      </div>
-    </MainLayout>
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={process.env.PUBLIC_URL} component={Dashboard} />
+          <Route exact path={process.env.PUBLIC_URL + '/login'} component={Login} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables'} component={Tables} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/:id'} component={TablesBookingId} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/booking/new'} component={TablesBookingNew} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/events/:id'} component={TablesEventsId} />
+          <Route exact path={process.env.PUBLIC_URL + '/tables/events/new'} component={TablesEventsNew} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter'} component={Waiter} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/:id'} component={WaiterOrderId} />
+          <Route exact path={process.env.PUBLIC_URL + '/waiter/order/new'} component={WaiterOrderNew} />
+          <Route exact path={process.env.PUBLIC_URL + '/kitchen'} component={Kitchen} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,9 +1,11 @@
 import React from 'react';
 //import styles from './MainLayout.scss';
 import PropTypes from 'prop-types';
+import PageNav from '../PageNav/PageNav';
 
 const MainLayout = (props) => (
   <div>
+    <PageNav/>
     {props.children}
   </div>
 );
