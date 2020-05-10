5ecc7eafb01dd109373b1d8cc0cf7d9275188514
Wojtek Nowak <billnewmann@gmail.com>
2020-05-10 18:38:13 +0100

    Add basic styling

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables</h2>
-    <Link to={`${process.env.PUBLIC_URL}/tables/bookingNew`}>+ New Booking</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/booking/123abc`}>Booking: 123abc</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/eventsNew`}>+ New Event</Link>
-    <Link to={`${process.env.PUBLIC_URL}/tables/events/123abc`}>Event: 123abc</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/bookingNew`} className={styles.link}>+ New Booking</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/booking/123abc`} className={styles.link}>Booking: 123abc</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/eventsNew`} className={styles.link}>+ New Event</Link>
+    <Link to={`${process.env.PUBLIC_URL}/tables/events/123abc`} className={styles.link}>Event: 123abc</Link>
   </div>
 );
 
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Waiter = () => (
   <div className={styles.component}>
     <h2>Waiter</h2>
-    <Link to={`${process.env.PUBLIC_URL}/waiter/orderNew`}>+ New Order</Link>
-    <Link to={`${process.env.PUBLIC_URL}/waiter/order/123abc`}>Order: 123abc</Link>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/orderNew`} className={styles.link}>+ New Order</Link>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/123abc`} className={styles.link}>Order: 123abc</Link>
   </div>
 );
 
